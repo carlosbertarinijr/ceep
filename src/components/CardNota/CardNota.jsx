dd0e085d1ab7a18d4c4f138f09cd9055553ff2fa
carlosbertarinijr <carlos.junior@codetec.io>
2020-10-09 13:56:38 -0300

    Importanto SVGs

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import "./estilo.css"
+import {ReactComponent as DeleteSVG} from "../../assets/img/delete.svg"
+
 class CardNota extends Component {
-    state = {  }
     render() { 
         return ( 
             <section className="card-nota">
                 <header className="card-nota_cabecalho">
                     <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+                    <DeleteSVG />
                 </header>
                 <p className="card-nota_texto">{this.props.texto}</p>
             </section>
