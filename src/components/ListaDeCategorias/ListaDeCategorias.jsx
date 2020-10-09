a64bbe8cdb2bc3f8806958cffe92be0501a03bcb
carlosbertarinijr <carlos.junior@codetec.io>
2020-10-09 15:32:41 -0300

    estilizacao ListaDeCategorias

--- src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -11,16 +11,19 @@ class ListaDeCategorias extends Component {
     render() {
         return (
             <section className="lista-categorias">
-                <ul>
+                <ul className="lista-categorias_lista">
                     {
                         this.props.categorias.map((categoria, index) => {
-                            return <li key={index}>{categoria}</li>
+                            return <li key={index} className="lista-categorias_item">
+                                {categoria}
+                            </li>
                         })
                     }
                 </ul>
                 <input
                     type="text"
                     placeholder='Adicionar categoria'
+                    className="lista-categorias_input"
                     onKeyUp={this._handleEnventoInput.bind(this)}
                 />
             </section>
